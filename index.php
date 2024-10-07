75c323d9d37c5d0422f4ec9c4fed9b295f6d68e7
GIMMI42PIASTRATO <67071988+GIMMI42PIASTRATO@users.noreply.github.com>
2024-10-07 17:46:26 +0200

    Add initial HTML and CSS files

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Vittorio Bussano</title>
+    <link rel="stylesheet" href="style.css">
+    <script src="script.js" defer></script>
+</head>
+
+<body>
+    <section>
+        <h1 class="name"><span class="capital">V</span>ittorio <span class="capital">B</span>ussano</h1>
+        <h1>Indipendent Front End</h1>
+        <h1>DEVELOPER</h1>
+        <h1>CURRENTLY UNENPLOYED D:< </h1>
+                <h1>FOLIO2024</h1>
+    </section>
+</body>
+
+</html>
