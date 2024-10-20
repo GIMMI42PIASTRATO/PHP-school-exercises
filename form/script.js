1c55904504a8ad78ba4e8976d29e5a6dd13164af
GIMMI42PIASTRATO <67071988+GIMMI42PIASTRATO@users.noreply.github.com>
2024-10-20 11:52:02 +0200

    Add the cool table like the one on the landing page for the index of exercises on the forms

---
+++ form/script.js
@@ -0,0 +1,30 @@
+document.querySelectorAll(".tableRow").forEach((row) => {
+	const hoverBackground = row.querySelector(".hoverBackground");
+	const projectPath = row.getAttribute("data-path");
+
+	row.addEventListener("click", () => {
+		window.open(projectPath, "_blank");
+	});
+
+	row.addEventListener("mouseenter", () => {
+		hoverBackground.style.top = "0";
+	});
+
+	row.addEventListener("mouseleave", (event) => {
+		const rowRect = row.getBoundingClientRect();
+
+		// Get the mouse position relative to the row
+		console.log("event.clientY:", event.clientY);
+		console.log("rowRect.top:", rowRect.top);
+		const mouseY = event.clientY - rowRect.top;
+		console.log("🖱️:", mouseY);
+
+		// If the mouse cursor is above the half of the row, move the hover background to -100%
+		// Otherwise, move it to 100%
+		if (mouseY < rowRect.height / 2) {
+			hoverBackground.style.top = "-100%";
+		} else {
+			hoverBackground.style.top = "100%";
+		}
+	});
+});
