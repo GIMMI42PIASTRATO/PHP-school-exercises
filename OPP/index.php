f311d4e09fd9f1043f5549d30835f245cf7fcbe5
GIMMI42PIASTRATO <67071988+GIMMI42PIASTRATO@users.noreply.github.com>
2024-10-28 01:39:22 +0100

    Add index for OOP exercises

---
+++ OPP/index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Indice Esercizi Form</title>
+    <link rel="stylesheet" href="./style.css">
+    <script src="./script.js" defer></script>
+</head>
+
+<body>
+    <div class="table">
+        <div class="tableHeader">
+            <p>Exercise</p>
+            <p>Category</p>
+            <p>Complexity</p>
+        </div>
+
+        <?php
+        $projects = [
+            [
+                "id" => 1,
+                "path" => "./es1",
+                "projectName" => "Esercizio 1",
+                "category" => "Classes",
+                "complexity" => "Easy ASF",
+            ],
+            [
+                "id" => 2,
+                "path" => "./es2",
+                "projectName" => "Esercizio 2",
+                "category" => "Classes",
+                "complexity" => "Easy ASF",
+            ],
+            [
+                "id" => 3,
+                "path" => "./es3",
+                "projectName" => "Esercizio 3",
+                "category" => "Classes",
+                "complexity" => "Easy ASF",
+            ],
+        ];
+        ?>
+
+        <?php foreach ($projects as $project) : ?>
+            <div class='rowDividier'></div>
+            <div class='tableRow' data-path='<?= htmlspecialchars($project['path']) ?>'>
+                <p><?= $project['projectName'] ?></p>
+                <p><?= $project['category'] ?></p>
+                <p class='projectDate'><?= $project["complexity"] ?></p>
+                <div class='hoverBackground'></div>
+            </div>
+        <?php endforeach  ?>
+    </div>
+</body>
+
+</html>
