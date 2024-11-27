adf6ab63e7b8c5a1539b9e7e8e4ac308d62aab04
GIMMI42PIASTRATO <67071988+GIMMI42PIASTRATO@users.noreply.github.com>
2024-11-27 13:16:15 +0100

    feat(class/Memory): implement Memory class for storing and managing calculator memory

---
+++ calculator/class/Memory.php
@@ -0,0 +1,31 @@
+<?php
+declare(strict_types=1);
+
+class Memory {
+    private array $memory = [];
+
+    public function __construct()
+    {
+        $this->memory = [];
+    }
+
+    public function add(int $value): void {
+        array_push($this->memory, $value);
+    }
+
+    public function remove(): void {
+        array_pop($this->memory);
+    }
+
+    public function recall(): int {
+        return $this->memory[count($this->memory) - 1];
+    }
+
+    public function get(): array {
+        return $this->memory;
+    }
+
+    public function set(array $memory): void {
+        $this->memory = $memory;
+    }
+}
