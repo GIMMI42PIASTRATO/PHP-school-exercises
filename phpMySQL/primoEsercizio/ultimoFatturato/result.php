97eb74d5d03ce7b06a7148748a8d4a3c35fa3a44
GIMMI42PIASTRATO <67071988+GIMMI42PIASTRATO@users.noreply.github.com>
2025-01-08 00:51:50 +0100

    feat(primoEsercizio): enhance UI and structure for ultimoFatturato and home pages

--- phpMySQL/primoEsercizio/ultimoFatturato/result.php
+++ phpMySQL/primoEsercizio/ultimoFatturato/result.php
@@ -31,56 +31,58 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 </head>
 
 <body>
-    <?php if ($_SERVER['REQUEST_METHOD'] === 'POST') : ?>
-        <?php
-        $stmt->execute();
-        $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
-        ?>
+    <div class="mainWrapper">
+        <?php if ($_SERVER['REQUEST_METHOD'] === 'POST') : ?>
+            <?php
+            $stmt->execute();
+            $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
+            ?>
 
-        <h1>Risultati</h1>
-        <p>Rappresentati con un fatturato compreso tra <?= $fatturatoMin ?> e <?= $fatturatoMax ?></p>
+            <h1>Risultati</h1>
+            <p>Rappresentati con un fatturato compreso tra <?= $fatturatoMin ?> e <?= $fatturatoMax ?></p>
 
-        <table>
-            <thead>
-                <tr>
-                    <th>ID</th>
-                    <th>Nome</th>
-                    <th>Cognome</th>
-                    <th>Ultimo Fatturato</th>
-                    <th>Regione</th>
-                    <th>Provincia</th>
-                    <th>Percentuale provvigione</th>
-                </tr>
-            </thead>
-            <tbody>
-                <?php foreach ($stmt->fetchAll() as $rappresentante) : ?>
+            <table>
+                <thead>
                     <tr>
-                        <td><?= $rappresentante['id'] ?></td>
-                        <td><?= $rappresentante['nome'] ?></td>
-                        <td><?= $rappresentante['cognome'] ?></td>
-                        <td><?= $rappresentante['ultimo_fatturato'] ?></td>
-                        <td><?= $rappresentante['regione'] ?></td>
-                        <td><?= $rappresentante['provincia'] ?></td>
-                        <td><?= $rappresentante['percentuale_provvigione'] ?></td>
+                        <th>ID</th>
+                        <th>Nome</th>
+                        <th>Cognome</th>
+                        <th>Ultimo Fatturato</th>
+                        <th>Regione</th>
+                        <th>Provincia</th>
+                        <th>Percentuale provvigione</th>
                     </tr>
-                <?php endforeach ?>
-            </tbody>
-            <tfoot>
-                <tr>
-                    <td>Total row</td>
-                    <td><?= $stmt->rowCount() ?></td>
-                </tr>
-            </tfoot>
-        </table>
+                </thead>
+                <tbody>
+                    <?php foreach ($stmt->fetchAll() as $rappresentante) : ?>
+                        <tr>
+                            <td><?= $rappresentante['id'] ?></td>
+                            <td><?= $rappresentante['nome'] ?></td>
+                            <td><?= $rappresentante['cognome'] ?></td>
+                            <td><?= $rappresentante['ultimo_fatturato'] ?></td>
+                            <td><?= $rappresentante['regione'] ?></td>
+                            <td><?= $rappresentante['provincia'] ?></td>
+                            <td><?= $rappresentante['percentuale_provvigione'] ?></td>
+                        </tr>
+                    <?php endforeach ?>
+                </tbody>
+                <tfoot>
+                    <tr>
+                        <td colspan="6">Total row</td>
+                        <td><?= $stmt->rowCount() ?></td>
+                    </tr>
+                </tfoot>
+            </table>
 
 
-    <?php else : ?>
-        <h1>Errore 405 metodo non permesso</h1>
-        <p>Accedi a questa pagina utilizzando il metodo POST</p>
-        <?php
-        http_response_code(405);
-        ?>
-    <?php endif ?>
+        <?php else : ?>
+            <h1>Errore 405 metodo non permesso</h1>
+            <p>Accedi a questa pagina utilizzando il metodo POST</p>
+            <?php
+            http_response_code(405);
+            ?>
+        <?php endif ?>
+    </div>
 </body>
 
 </html>
