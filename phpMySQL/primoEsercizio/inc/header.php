de244fbaae7177d5359e4f3e205a78b0348b85dc
GIMMI42PIASTRATO <67071988+GIMMI42PIASTRATO@users.noreply.github.com>
2025-01-22 20:32:15 +0100

    refactor(primoEsercizio): restructure project by moving header to a separate file and updating index structure

---
+++ phpMySQL/primoEsercizio/inc/header.php
@@ -0,0 +1,58 @@
+<nav>
+    <div class="navLeft">
+        <span><a href="">💻 Company.io</a></span>
+        <span class="appsLink cursorPointer">
+            <a>Apps</a>
+            <object
+                class="chevronDown"
+                data="./assets/chevron-up.svg"
+                type="image/svg+xml"
+                width="20"
+                height="20"></object>
+        </span>
+        <span class="cursorPointer"><a
+                href="https://github.com/GIMMI42PIASTRATO/PHP-school-exercises">Contribute</a></span>
+    </div>
+    <div class="appPopup">
+        <div class="mainLink">
+            <span>💻</span>
+            <span>Company.io</span>
+            <span>Scopri i dati delle altre aziende per rimanere
+                sempre aggiornato</span>
+        </div>
+        <div
+            class="cursorPointer appLink"
+            data-link="ultimoFatturato/">
+            <span>Ultimo fatturato</span>
+            <span class="linkDescription">Guarda l'ultimo fatturato dei rappresentanti in
+                base al loro ultimo fatturato</span>
+        </div>
+        <div
+            class="cursorPointer appLink"
+            data-link="aumentaProvvigione/">
+            <span>Aumenta provvigione</span>
+            <span class="linkDescription">Aumenta la provvigione dei rappresentanti di una
+                regione</span>
+        </div>
+        <div
+            class="cursorPointer appLink"
+            data-link="aggiungiRappresentante/">
+            <span>Aggiungi rappresentante</span>
+            <span class="linkDescription">Aggiungi un nuovo rappresentante al database</span>
+        </div>
+        <div
+            class="cursorPointer appLink"
+            data-link="eliminaRappresentante/">
+            <span>Elimina rappresentante</span>
+            <span class="linkDescription">Elimina un rappresentante con il suo codice
+                fiscale</span>
+        </div>
+        <div
+            class="cursorPointer appLink"
+            data-link="visualizzaRappresentanti/">
+            <span>Visualizza rappresentanti</span>
+            <span class="linkDescription">Visualizza tutti i rappresentanti all'interno del
+                database</span>
+        </div>
+    </div>
+</nav>
---
+++ phpMySQL/primoEsercizio/index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Company.io</title>
+    <link rel="stylesheet" href="./style/style.css" />
+    <link rel="icon" href="./assets/favicon.ico" type="image/x-icon" />
+    <script src="./js/script.js" defer></script>
+</head>
+
+<body>
+    <header>
+        <?php include_once "./inc/header.php"; ?>
+    </header>
+    <main class="mainWrapper" style="padding-top: 10rem">
+        <div class="hero">
+            <div class="heroText">
+                <h1>Fai come le grandi aziende</h1>
+                <p>
+                    Scopri i dati delle altre aziende per rimanere sempre
+                    aggiornato sulle ultime tendenze del mercato
+                </p>
+            </div>
+            <div class="heroLinks">
+                <a href="./ultimoFatturato" class="heroLink">Prova Ora</a>
+                <a
+                    href="https://github.com/GIMMI42PIASTRATO/PHP-school-exercises">Contribute</a>
+            </div>
+            <div class="heroImage">
+                <img src="./assets/img0.png" alt="image" />
+            </div>
+        </div>
+    </main>
+</body>
+
+</html>
